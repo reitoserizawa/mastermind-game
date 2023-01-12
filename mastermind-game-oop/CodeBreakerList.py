397518870c18ba64f5440d2a610476f10b09d47a
ReitoSerizawa <s.reitiger@gmail.com>
2023-01-12 15:10:25 -0500

    code breaker list edited

--- mastermind-game-oop/CodeBreakerList.py
+++ mastermind-game-oop/CodeBreakerList.py
@@ -4,8 +4,7 @@ from CodeBreaker import CodeBreaker
 # this class holds the number of code breakers and each class in the list
 class CodeBreakerList:
     def __init__(self):
-        self.code_breaker_count = self.set_code_breaker_count()
-        self.code_breaker_list = self.set_code_breaker_list(self.code_breaker_count)
+        self.code_breaker_list = self.set_code_breaker_list()
 
     @staticmethod
     def set_code_breaker_count():
@@ -22,16 +21,16 @@ class CodeBreakerList:
                     print("Please type a number.")
                     continue
     
-    @staticmethod
-    def set_code_breaker_list(num):
-        player_list = []
+    def set_code_breaker_list(self):
         # if there is only one code breaker, simply ask for the code breaker's name
         # otherwise, ask each code breaker to input their name and add them to the list
-        if num == 1:
+        code_breaker_count = self.set_code_breaker_count()
+        if code_breaker_count == 1:
             name = input("Code breaker, please type your name: ")
             return [CodeBreaker(name)]
         else:
-            for i in range(num):
+            player_list = []
+            for i in range(code_breaker_count):
                 name = input(f'{__main__.make_ordinal(i+1)} code breaker, please type your name: ')
                 player_list.append(CodeBreaker(name))
             return player_list
